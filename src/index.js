52ece2383f07183ce75b63f83f10eec3ab4d5adc
kgorlov <kirillgorlov90@gmail.com>
2021-02-06 21:19:38 +0300

    Кнопки(Кирилл сделал)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
-function App()
+import React from "react"
+import {render} from "react-dom"
+
+function Stopwatch()
 {
     return(
         <div>
-            <h1>Саня нуб</h1>
+            <h1>0:0</h1>
+            <StartStopButton/>
+            <ResetButton/>
         </div>
     )
 }
-console.log(App());
+function StartStopButton()
+{
+    return(
+        <div>
+            <button>start</button>
+        </div>
+    )
+}
+function ResetButton()
+{
+    return(
+        <div>
+            <button>reset</button>
+        </div>
+    )
+}
+render(<Stopwatch/>, document.getElementById("root"));
