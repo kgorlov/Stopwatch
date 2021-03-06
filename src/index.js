834d53517f1149d95bdb3c7f9ff64f567ba178b4
kgorlov <kirillgorlov90@gmail.com>
2021-03-06 21:19:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,51 +2,113 @@
 import {render} from "react-dom"
 import React, {Component} from 'react'
 
-class StartStopButton extends Component {
-    state = {
-        isOpen: true
-    }
 
-    render() {
-        const {article} = this.props
-        const body = this.state.isOpen && <section>{article}</section>
-        return (
-            <div>
-                <h2>
-                    {article}
-                    <button onClick={this.handleClick}>
-                        {this.state.isOpen ? 'start' : 'stop'}
-                    </button>
-                </h2>
-                {body}
+class Stopwatch extends Component {
+
+    state = {
+        intervalId: null,
+        isStarted : false,
+        seconds : 0,
+        minutes : 0,
+        msecondss : 0
+    }
+    
+    render(){
+
+        return(
+            <div style={{textAlign: "center"}}>
+                <h1 style={{ padding: "10px 20px", textAlign: "center", color: "black"}}>{this.state.minutes}:{this.state.seconds}:{this.state.msecondss}</h1>
+
+                <StartStopButton
+                    isStarted={this.state.isStarted} 
+                    onStartBtn={this.handleClick.bind(this)}
+                />
+                <ResetButton
+                    onResetBtn={this.resetClick.bind(this)}
+                />
             </div>
         )
     }
 
     handleClick = () => {
         this.setState({
-            isOpen: !this.state.isOpen
+            //seconds: this.state.seconds + 1,
+            isStarted: !this.state.isStarted
         })
+        /*if (this.state.seconds == 59) {
+            this.setState({ seconds: 0,minutes: this.state.minutes + 1})
+        } else {
+            this.setState({ seconds: this.state.seconds + 1 });
+        }*/
+    }
+    resetClick = () => {
+        this.setState({
+            minutes : this.state.minutes = 0,
+            seconds : this.state.seconds = 0,
+            isStarted : !this.state.isStarted
+        })
+    }
+    componentDidMount() {
+
+        let intervalId = setInterval(this.timer.bind(this), 10);
+        // store intervalId in the state so it can be accessed later:
+        this.setState({intervalId: intervalId});
+     }
+     
+     timer() {
+        if(this.state.isStarted == true)
+        {
+            console.log('tick');
+            if (this.state.msecondss == 99) {
+                this.setState({ msecondss: 0,seconds: this.state.seconds + 1})
+            } else {
+                this.setState({ msecondss: this.state.msecondss + 1 });
+            }
+
+            if (this.state.seconds == 59) {
+                this.setState({ seconds: 0,minutes: this.state.minutes + 1});
+            }
+        }
     }
 }
 
-function Stopwatch()
-{
-    return(
-        <div>
-            <h1>0:0</h1>
-            <StartStopButton/>
-            <ResetButton/>
-        </div>
-    )
+
+class StartStopButton extends Component {
+
+    render() {
+        const {isStarted, onStartBtn} = this.props
+        return (
+            <div>
+                <h2>
+                    <button onClick={onStartBtn}>
+                        {isStarted ? 'stop' : 'start'}
+                    </button>
+                    
+                </h2>
+            </div>
+        )
+    }
+
+
 }
 
-function ResetButton()
-{
-    return(
-        <div>
-            <button>reset</button>
-        </div>
-    )
+
+class ResetButton extends Component {
+
+    render() {
+        const {onResetBtn} = this.props
+        return (
+            <div>
+                <h2>
+                    <button onClick={onResetBtn}>
+                        reset
+                    </button>
+                </h2>
+            </div>
+        )
+    }
+
+
 }
+
 render(<Stopwatch/>, document.getElementById("root"));
